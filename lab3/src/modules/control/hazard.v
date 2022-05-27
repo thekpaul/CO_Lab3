cb56b78dc7c75fdbc529e184cca67b58f3e1b609
Paul Kim <44695374+thekpaul@users.noreply.github.com>
2022-05-28 01:39:55 +0900

    Flush only if BP is INCORRECT

--- lab3/src/modules/control/hazard.v
+++ lab3/src/modules/control/hazard.v
@@ -9,6 +9,8 @@
 module hazard #(
   parameter DATA_WIDTH = 32
 )(
+  input rstn,
+
   input [4 : 0] id_rs1,
   input [4 : 0] id_rs2,
   input [6 : 0] opcode,
@@ -40,38 +42,49 @@ module hazard #(
 */
 
 initial begin
-  flush <= 1'b0;
-  stall <= 1'b0;
+  flush = 1'b0;
+  stall = 1'b0;
 end
 
 reg [2 : 0] use_rs = 3'bXXX; // use_rs1 = use_rs[1], use_rs2 = use_rs[2]
 
 always @(*) begin
 
-  // FLUSH
-  if (pc_plus_4 != pc_target) flush <= 1'b1;
-  else flush <= 1'b0;
+  if (rstn == 1'b0) begin
 
-  // USAGE Determination
-  casex (opcode)
-    7'b011_0011: use_rs <= 3'b110; // R
-    7'b001_0011: use_rs <= 3'b010; // I
-    7'b000_0011: use_rs <= 3'b010; // L
-    7'b010_0011: use_rs <= 3'b110; // S
-    7'b110_0011: use_rs <= 3'b110; // B
-    7'b110_1111: use_rs <= 3'b000; // J
-    7'b110_0111: use_rs <= 3'b010; // JALR
-    7'b011_0111: use_rs <= 3'b000; // LUI
-    7'b001_0111: use_rs <= 3'b000; // AUIPC
-    default:     use_rs <= 3'bXXX; // ERROR
-  endcase
+    flush = 1'b0;
+    stall = 1'b0;
 
-  // STALL
-  if ( (use_rs[1] == 1'b1 && (id_rs1 != 5'b00000) &&
-          (id_rs1 ==  ex_rd) &&  ex_memread == 1'b1 ) ||
-       (use_rs[2] == 1'b1 && (id_rs2 != 5'b00000) &&
-          (id_rs2 ==  ex_rd) &&  ex_memread == 1'b1 ) ) stall <= 1'b1;
-  else stall <= 1'b0;
+  end else begin
+
+    // FLUSH
+    if ((pc_plus_4 !== 32'hXXXX_XXXX) && (pc_target !== 32'hXXXX_XXXX) &&
+        (pc_plus_4 !== pc_target)) flush = 1'b1;
+    else begin
+      flush = 1'b0;
+
+      // USAGE Determination
+      casex (opcode)
+        7'b011_0011: use_rs = 3'b110; // R
+        7'b001_0011: use_rs = 3'b010; // I
+        7'b000_0011: use_rs = 3'b010; // L
+        7'b010_0011: use_rs = 3'b110; // S
+        7'b110_0011: use_rs = 3'b110; // B
+        7'b110_1111: use_rs = 3'b000; // J
+        7'b110_0111: use_rs = 3'b010; // JALR
+        7'b011_0111: use_rs = 3'b000; // LUI
+        7'b001_0111: use_rs = 3'b000; // AUIPC
+        default:     use_rs = 3'bXXX; // ERROR
+      endcase
+
+      // STALL
+      if ( (use_rs[1] == 1'b1 && (id_rs1 != 5'b00000) &&
+              (id_rs1 ==  ex_rd) &&  ex_memread == 1'b1 ) ||
+           (use_rs[2] == 1'b1 && (id_rs2 != 5'b00000) &&
+              (id_rs2 ==  ex_rd) &&  ex_memread == 1'b1 ) ) stall = 1'b1;
+      else stall = 1'b0;
+    end
+  end
 end
 
 endmodule
